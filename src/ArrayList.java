6d7393aa515f510ee6f0bdbcd278ef65a172cbf5
Mareks Zeile <mzeile23@mzeile23.local>
2021-10-28 08:39:32 -0600

    added an arraylist to the class

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -1,2 +1,14 @@
+/**
+ ArrayList
+ The program will create an arraylist that uses
+ Author: Mareks Zeile
+ Collaborator(s):
+ Collaboration:
+ Date: 10/28/21
+ **/
 public class ArrayList {
+    public static void main(String[] args)
+    {
+        ArrayList arr = new ArrayList();
+    }
 }
