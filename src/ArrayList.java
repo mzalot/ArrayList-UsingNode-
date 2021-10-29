ba1966c24ef7b13bf5714ecbc8ab83688ad47533
Mareks Zeile <mzeile23@mzeile23.local>
2021-10-29 14:31:18 -0600

    added my methods to the arraylist class

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -6,20 +6,35 @@
  Collaboration:
  Date: 10/28/21
  **/
-public class ArrayList {
-    //intance data
+public class ArrayList <T>{
+    //instance data
     NodeProject node = new NodeProject();
     //counter for index of node
     int counter = 0;
 
     //void add method
-    void add(int i, T data){
-        node.setNode1(NodeProject newNode = new NodeProject())
+    public void add(int index, T data){
+        //node to find current node
+        NodeProject currentNode = node;
+        for(int i = 0; i< index; i++){
+            currentNode = currentNode.getNode1();
+        }
+        //add the new node with the data
+        NodeProject nodeAdd = new NodeProject(data);
+        //node we are replacing and moving
+        NodeProject moveNode = currentNode.getNode1();
+        nodeAdd.setNode1(currentNode);
+        nodeAdd.setNode2(currentNode);
+        //increase counter
+        counter++;
+
+
     }
 
     //int size method
-    public static void main(String[] args)
-    {
-
+    public int size(){
+        return counter;
+    }
+    public static void main(String[] args) {
     }
 }
