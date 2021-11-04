67357c6d9866ce8c7007f226fdaf00a1ca3d0086
mzalot <69329263+mzalot@users.noreply.github.com>
2021-11-03 19:37:54 -0600

    correcting format of main

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -7,19 +7,8 @@
  Date: 10/28/21
  **/
 public class ArrayList <T>{
-    //intance data
-    NodeProject node = new NodeProject();
+    //instance data
+    private NodeProject node;
     //counter for index of node
-    int counter = 0;
-
-    //void add method
-    void add(int i, T data){
-        node.setNode1(NodeProject newNode = new NodeProject())
-    }
-
-    //int size method
-    public static void main(String[] args)
-    {
-
-    }
+    private int counter;
 }
